955af9a364153c5729a1fed1bdb6b0ea4947b3fb
FABRIZZIO <FABRIZZIO@DESKTOP-8PLVDLM>
2023-09-21 22:47:45 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 1 Semana 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1_Semana_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el primer numero:");
+            int num_1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero:");
+            int num_2 = int.Parse(Console.ReadLine());
+            int producto = 0;
+
+            for (int i = 0; i < num_2; i++)
+            {
+                producto += num_1;
+            }
+
+            Console.WriteLine($"el producto es: {producto}");
+            Console.ReadKey();
+        }
+    }
+}
